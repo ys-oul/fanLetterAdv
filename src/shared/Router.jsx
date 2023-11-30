4a54832eda0d37e728dd2f4beae6c8a5dcf41382
yesol <cloud.mys.99@gmail.com>
2023-11-30 15:13:48 +0900

    router 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,7 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import SignUp from "pages/SignUp";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
@@ -9,6 +11,8 @@ export default function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="*" element={<Navigate replace to="/" />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
